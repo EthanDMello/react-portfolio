7e6096788f5763c91a3550ef9efc6ec0fbfcb4a8
TheBiggestThereIs <107923914+EthanDMello@users.noreply.github.com>
2022-11-21 16:20:01 +0000

    adding styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 // TODO: Add a comment explaining what this import statement is doing
 import Header from "./components/Header";
+import "./App.ccs";
 
 // TODO: Add a comment explaining the purpose of the App component
 function App() {
