b769b91bb2271c5284b71a1321721a3d561d8502
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-12-06 20:19:40 +0000

    import css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 // import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Project from "./components/Project";
-// import "./App.ccs";
+import "./App.css";
 
 function App() {
   return (
